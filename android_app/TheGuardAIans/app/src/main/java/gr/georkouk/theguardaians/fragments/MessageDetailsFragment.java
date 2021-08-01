7729c2440ea2663833c58a9987093018f09d8991
George Koukouris <georgekouk27@gmail.com>
2021-08-01 15:15:11 +0300

    feat: Added left/right swipe navigation at the message details screen
    Fix #5

--- android_app/TheGuardAIans/app/src/main/java/gr/georkouk/theguardaians/fragments/MessageDetailsFragment.java
+++ android_app/TheGuardAIans/app/src/main/java/gr/georkouk/theguardaians/fragments/MessageDetailsFragment.java
@@ -1,34 +1,25 @@
 package gr.georkouk.theguardaians.fragments;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.content.ContextWrapper;
-import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.os.Environment;
-import android.util.Base64;
-import android.view.ContextThemeWrapper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
-import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
-
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.bumptech.glide.request.RequestOptions;
-
-import java.io.File;
-
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -36,6 +27,7 @@ import gr.georkouk.theguardaians.NotificationActivity;
 import gr.georkouk.theguardaians.R;
 import gr.georkouk.theguardaians.dao.DaoMessage;
 import gr.georkouk.theguardaians.models.Message;
+import gr.georkouk.theguardaians.utils.OnSwipeTouchListener;
 
 
 public class MessageDetailsFragment extends Fragment {
@@ -47,10 +39,12 @@ public class MessageDetailsFragment extends Fragment {
     @BindView(R.id.tvStatus) TextView tvStatus;
     @BindView(R.id.tvTime) TextView tvTime;
     @BindView(R.id.imageView) ImageView imageView;
+    @BindView(R.id.messageDetailsRootLayout) ConstraintLayout messageDetailsRootLayout;
 
     private Message message;
 
 
+    @SuppressLint("ClickableViewAccessibility")
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -60,6 +54,27 @@ public class MessageDetailsFragment extends Fragment {
         View view = inflater.inflate(R.layout.message_view, container, false);
 
         ButterKnife.bind(this, view);
+
+        OnSwipeTouchListener onSwipeTouchListener = new OnSwipeTouchListener(getActivity()){
+            public void onSwipeTop() {
+
+            }
+            public void onSwipeRight() {
+                togglePreviousOrNextMessage(DaoMessage.PREVIOUS_MESSAGE);
+            }
+            public void onSwipeLeft() {
+                togglePreviousOrNextMessage(DaoMessage.NEXT_MESSAGE);
+            }
+            public void onSwipeBottom() {
+
+            }
+            public void singleTapUp(){
+                imageView.performClick();
+            }
+        };
+
+        messageDetailsRootLayout.setOnTouchListener(onSwipeTouchListener);
+        imageView.setOnTouchListener(onSwipeTouchListener);
 
         if(NotificationActivity.id > 0){
             fillScreen(
@@ -119,6 +134,19 @@ public class MessageDetailsFragment extends Fragment {
     public void setMessageSeen(){
         DaoMessage.setMessageSeen(this.message.getId());
 
+        clearScreen();
+        Message nextMessage = DaoMessage.getPreviousOrNextMqttMessage(message.getId(), DaoMessage.NEXT_MESSAGE);
+        if(nextMessage.getId() > 0){
+            fillScreen(nextMessage);
+            return;
+        }
+
+        Message prevMessage = DaoMessage.getPreviousOrNextMqttMessage(message.getId(), DaoMessage.PREVIOUS_MESSAGE);
+        if(prevMessage.getId() > 0){
+            fillScreen(prevMessage);
+            return;
+        }
+
         back();
     }
 
@@ -157,6 +185,23 @@ public class MessageDetailsFragment extends Fragment {
                 .load(imageFile)
                 .apply(options)
                 .into(image);
+    }
+
+    private void togglePreviousOrNextMessage(int scrollType){
+        Message newMessage = DaoMessage.getPreviousOrNextMqttMessage(message.getId(), scrollType);
+        if(newMessage.getId() > 0){
+            clearScreen();
+            fillScreen(newMessage);
+        }
+        else{
+            Toast.makeText(
+                    getContext(),
+                    scrollType == DaoMessage.PREVIOUS_MESSAGE ?
+                            "You have reached the beginning of the message list." :
+                            "You have reached the end of the message list.",
+                    Toast.LENGTH_LONG
+            ).show();
+        }
     }
 
 }
